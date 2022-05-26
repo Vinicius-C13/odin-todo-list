1913e83cd862c2c566e0efa63901df631c97d15d
vinicius <viniciuscosta.us@gmail.com>
2022-05-26 17:01:10 -0300

    Finished the system to create a project

--- src/form/project-form.js
+++ src/form/project-form.js
@@ -1,15 +1,20 @@
 const projectForm = `
-    <form class="create-new" id="project-form">
-        <div class="create-new--header">Create a new project<div id="close-form-btn"></div></div>
-        <div class="create-new--content">
-                <label for="new-todo--date">Project Name</label>
+        <form class="create-new">
+            <div class="create-new--header">Create a new project<div id="close-form-btn"></div></div>
+            <div class="create-new--content">
+                <label for="new-project--name">Project Name:</label>
                 <input type="text" id="new-project--name" name="new-project" required>
+                <label for="new-project--desc">Details:</label>
+                <textarea id="new-project--desc" name="new-project" maxlength="80"></textarea>
+                <div class="wrap-priority-submit">
+                    <div>
+                        <label for="new-project--color">Color:</label>
+                        <input type="color" id="new-project--color" name="new-project" required>
+                    </div>
+                    <button>Create</button>
+                </div>
             </div>
-            <div class="wrap-priority-submit">
-                <button>Create</button>
-            </div>
-        </div>
-    </form>
+        </form>
 `;
 
 export {projectForm};
--- src/index.js
+++ src/index.js
@@ -2,29 +2,28 @@ import {projectForm} from './form/project-form.js';
 import {taskForm} from './form/task-form.js';
 
 //This is just temporary - remove as soon as possible;
-function displayForm () {
-    const form = document.createElement('div');
-    form.innerHTML = projectForm;
-    document.getElementById('main').appendChild(form);
-};
-displayForm();
 
 const allTasks_array = [];
 const doneTasks_array = [];
 const todayTasks_array = [];
 const allProjects_array = [];
 
-const projectFactory = (name) => {
-    return {name}
+const projectFactory = (name, color, details) => {
+    const addATask = (task) => {
+        relatedTasks.unshift(task);
+    }
+    const relatedTasks = ['pirilampos'];
+    return {name, color, details, relatedTasks, addATask};
 }
 
-const Logic = (()=>{
-    const createNewProject = (name) => {
-        const newProject = projectFactory(name);
+const Logic = (() => {
+    const createNewProject = (name, color, details) => {
+        const newProject = projectFactory(name, color, details);
+        const parentEl = document.querySelector('.projects-list');
 
         _addProjectToArray(newProject);
 
-        UI.displayProject(newProject);
+        UI.displayElement(newProject, parentEl);
     }
 
     const _addProjectToArray = (project) => {
@@ -35,19 +34,77 @@ const Logic = (()=>{
     return {createNewProject}
 })();
 
-const UI = (()=>{
+const UI = (() => {
 
-    const displayProject = (project)=> {
-        console.log(project);
+    const openForm = (type) => {
+        if (type === 'project') {
+            const overlay = document.querySelector('#overlay-project');
+            overlay.classList.add('show-overlay');
+            overlay.innerHTML = projectForm;
+        } else if (type === 'task') {
+            const overlay = document.querySelector('#overlay-task');
+            overlay.classList.add('show-overlay');
+            overlay.innerHTML = taskForm;
+        }
+    }
+
+    const closeForm = (type) => {
+        if (type === 'project') {
+            const overlay = document.querySelector('#overlay-project');
+            overlay.classList.remove('show-overlay');
+            overlay.innerHTML = "";
+        } else if (type === 'task') {
+            const overlay = document.querySelector('#overlay-task');
+            overlay.classList.remove('show-overlay');
+            overlay.innerHTML = "";
+        }
+    }
+
+    const _createProjectElement = (project) => {
+        const projectEl = document.createElement('li');
+        projectEl.classList.add('project-item', 'btn-pointer')
+        projectEl.obj_ID = project;
+        projectEl.innerHTML = `<div class= "project-color" style="background: ${project.color}"></div>${project.name}`;
+        console.log(projectEl)
+        return projectEl
+    }
+
+    const displayElement = (project, parent)=> {
+        parent.prepend(_createProjectElement(project));
     };
-    return {displayProject}
+
+    return {openForm, closeForm, displayElement}
 })();
 
 //Events tab
-document.querySelector('#project-form').addEventListener('submit', (e)=>{
+document.querySelector('#overlay-project').addEventListener('submit', (e)=>{
     e.preventDefault();
 
     const projectName = document.querySelector('#new-project--name').value;
+    const projectColor = document.querySelector('#new-project--color').value;
+    const projectDetails = document.querySelector('#new-project--desc').value;
 
-    Logic.createNewProject(projectName);
+    Logic.createNewProject(projectName, projectColor, projectDetails);
+
+    UI.closeForm('project');
+});
+
+document.querySelector('.projects-list').addEventListener('click', (e) => {
+    if(e.target.classList.contains('project-item')) {
+        e.target.obj_ID.addATask('teste');
+    };
+})
+
+//Event: Open project form
+document.querySelector('.new-list-btn').addEventListener('click', () => {
+    UI.openForm('project');
+    console.log('teste');
+})
+
+//Event: Close project form
+document.querySelector('#overlay-project').addEventListener('click', (e) => {
+    if(e.target.id === 'close-form-btn'){
+        console.log('raraar')
+        UI.closeForm('project');
+    }
 });
